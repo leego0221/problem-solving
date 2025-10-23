2b0c520fd61fca51a0532eb9b9be35ace5db2364
leego0221 <86773648+leego0221@users.noreply.github.com>
2025-10-23 16:17:48 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n, x;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	cin >> n;
+	while (n--) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) {
+				cout << "0\n";
+			}
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+	return 0;
+}
