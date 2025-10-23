b844bdd22315391bfcb214d08a732ea5e0d6c6fd
leego0221 <86773648+leego0221@users.noreply.github.com>
2025-10-23 17:04:20 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 8 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
@@ -0,0 +1,8 @@
+#include <vector>
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+    std::vector<int>::iterator iter;
+	for (iter = a.begin(); iter != a.end(); iter++)
+		ans += *iter;
+	return ans;
+}
