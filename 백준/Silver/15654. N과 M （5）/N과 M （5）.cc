514225828feaf3c1ab3ad86f0483e5e3c02efce2
leego0221 <86773648+leego0221@users.noreply.github.com>
2025-10-23 17:06:04 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int sequence[9], ans[9];
+bool visited[9];
+
+void backtracking(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			ans[cnt] = sequence[i];
+			visited[i] = true;
+			backtracking(cnt + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> sequence[i];
+	}
+	sort(sequence, sequence + n);
+	backtracking(0);
+	return 0;
+}
