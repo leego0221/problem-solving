ed7a181bb0d2f0885ffe74029a37a89b8d20a184
leego0221 <86773648+leego0221@users.noreply.github.com>
2025-10-23 17:04:49 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int ans[9];
+
+void backtracking(int cnt, int start) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = start; i <= n; i++) {
+		ans[cnt] = i;
+		backtracking(cnt + 1, i + 1);
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> m;
+	backtracking(0, 1);
+	return 0;
+}
